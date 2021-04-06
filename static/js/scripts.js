dec7e3ff2d4385692626349e014666479cac05ef
Patrick Dugan <dugan.pat@gmail.com>
2021-04-06 04:09:31 -0400

    Switches autoplay to play buttons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,19 @@
 $(function() {
     $('.image_wrapper').fluidbox();
 });
+
+function playVideo(video) {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+$(function(){
+   $('.play_link').click(function () {
+      $(this).text(function(i, text){
+          return text === 'Play video' ? 'Pause video' : 'Play video';
+      })
+   });
+})
